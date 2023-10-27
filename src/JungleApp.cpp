5fdf3dd7bbc8a6c4f1305ec1c915301095bcd87b
Ilia Bozhinov <ammen99@gmail.com>
2023-10-27 17:03:26 +0200

    JungleApp: allow non-discrete GPUs
    I do not have a dGPU :(

--- src/JungleApp.cpp
+++ src/JungleApp.cpp
@@ -284,8 +284,7 @@ bool JungleApp::isDeviceSuitable(VkPhysicalDevice const &device) {
         swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
     }
 
-    return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-           deviceFeatures.geometryShader &&
+    return deviceFeatures.geometryShader &&
            indices.isComplete() &&
            extensionsSupported &&
            swapChainAdequate;
